26c5ba090d80dcf108825c1613574785cf693829
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2019-05-02 09:23:41 -0500

    lazy load images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LazyLoadImagesModule } from 'ngx-lazy-load-images';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LazyLoadImagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
